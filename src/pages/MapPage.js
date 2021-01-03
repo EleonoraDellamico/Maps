0b4caadea492d90628222be528b61ca0cdbbf7a6
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-01-03 19:50:54 +0100

    set coords

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -4,13 +4,14 @@ import mapboxgl from "mapbox-gl";
 //CHANGE KEY
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ2l1bGlvNzgiLCJhIjoiY2tqaGNneGtiOTk1MzJ2cWpyemljZHQzcSJ9.HnFfA7OBcnwbaFJq-vB-5w';
 const firstSpot = {
-    lng: 5,
-    lat: 34,
-    zoom: 2
+    lng: 144.8794,
+    lat: -37.8020,
+    zoom: 11.52
 }
 export const MapPage = () => {
     const mapDiv = useRef();
     const[mapa, setMap]= useState();
+     const[coords, setCoords]= useState(firstSpot);
    
     useEffect(()=>{
         const map = new mapboxgl.Map({
@@ -21,8 +22,26 @@ export const MapPage = () => {
 });
     setMap(map);
     },[]);
+
+//MOVING MAPA
+    useEffect (()=>{
+        mapa?.on("move",()=>{
+            console.log("hey");
+            const{lng, lat} = mapa.getCenter();
+            console.log(lng.lat);
+            setCoords({
+                lng: lng.toFixed(4),
+                lat: lat.toFixed(4),
+                zoom:mapa.getZoom().toFixed(2)
+            })
+        });
+
+    },[mapa]);
     return (
         <>
+        <div className = "info">
+            lng:{coords.lng} | lat:{coords.lat} | zoom {coords.zoom}
+        </div>
         <div
           ref= {mapDiv}
           className="mapContainer"
