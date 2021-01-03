4f13762b8fa36e718e3a2e23bbb19fcb36e60c8c
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-01-03 17:52:16 +0100

    first commit

---
+++ src/MapsApp.js
@@ -0,0 +1,11 @@
+import React from "react"; 
+
+export const MapsApp = () => {
+    return (
+        <div>
+            <h1>MAPS</h1>
+        </div>
+    )
+
+}
+export default MapsApp; 
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MapsApp from './MapsApp';
+
+
+ReactDOM.render(
+
+    <MapsApp />,
+
+  document.getElementById('root')
+);
+
